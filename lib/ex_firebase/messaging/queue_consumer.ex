1f2d9aedf5b232500cadc1d4d3a6e59fd409ee00
Ben Hansen <bnhansn@gmail.com>
2018-12-09 21:25:21 -0800

    send queue results through gproc pub/sub

--- lib/ex_firebase/messaging/queue_consumer.ex
+++ lib/ex_firebase/messaging/queue_consumer.ex
@@ -1,14 +1,11 @@
 defmodule ExFirebase.Messaging.QueueConsumer do
   @moduledoc """
   The final consumer in the Queue GenStage pipeline.
-  It sends FCM requests and casts results to :queue_monitor.
+  It sends FCM requests and casts results to QueueMonitor.
   """
 
-  alias ExFirebase.{Error, Messaging}
+  alias ExFirebase.Messaging
   alias ExFirebase.Messaging.QueueMonitor
-
-  @queue_monitor Application.get_env(:ex_firebase, :queue_monitor) || QueueMonitor
-  @retry_offset_seconds 60
 
   def start_link(payload) do
     Task.start_link(fn ->
@@ -25,58 +22,10 @@ defmodule ExFirebase.Messaging.QueueConsumer do
   end
 
   defp send_message(payload) do
-    @queue_monitor.fcm_request(payload)
+    QueueMonitor.fcm_request(payload)
 
     payload
     |> Messaging.send()
-    |> parse_response(payload)
-    |> handle_result()
-  end
-
-  defp parse_response(
-         {:ok, %HTTPoison.Response{body: body, status_code: 200}},
-         payload
-       ) do
-    {:ok, body, payload}
-  end
-
-  defp parse_response(
-         {:ok,
-          %HTTPoison.Response{
-            body:
-              %{
-                "error" => %{
-                  "status" => "UNAVAILABLE"
-                }
-              } = body
-          }},
-         payload
-       ) do
-    {:retry, body, payload}
-  end
-
-  defp parse_response({:ok, %HTTPoison.Response{} = error}, payload) do
-    {:error, error, payload}
-  end
-
-  defp parse_response({:error, %HTTPoison.Error{} = error}, payload) do
-    {:error, error, payload}
-  end
-
-  defp parse_response({:error, %Error{} = error}, payload) do
-    {:error, error, payload}
-  end
-
-  defp handle_result({:ok, body, payload}) do
-    @queue_monitor.fcm_response({:ok, body, payload})
-  end
-
-  defp handle_result({:retry, body, payload}) do
-    @queue_monitor.fcm_response({:retry, body, payload})
-    Messaging.schedule(payload, @retry_offset_seconds)
-  end
-
-  defp handle_result({:error, error, payload}) do
-    @queue_monitor.fcm_response({:error, error, payload})
+    |> QueueMonitor.fcm_response(payload)
   end
 end
--- lib/ex_firebase/messaging/queue_monitor.ex
+++ lib/ex_firebase/messaging/queue_monitor.ex
@@ -1,34 +1,62 @@
 defmodule ExFirebase.Messaging.QueueMonitor do
   @moduledoc """
-  GenStage process that intercepts all request and responses from QueueConsumer
+  GenServer process that listens to events from QueueConsumer to track metrics.
+  Events are simultaneously broadcast through the `:gproc` registered process `:fcm_queue_monitor`.
   """
 
   use GenServer
 
-  @type fcm_result ::
-          {:ok, map(), map()}
-          | {:retry, map(), map()}
-          | {:error, any(), map()}
+  @type state :: %{
+          attempts: integer(),
+          successes: integer(),
+          failures: integer()
+        }
 
   def start_link(args) do
     GenServer.start_link(__MODULE__, args, name: __MODULE__)
   end
 
+  @impl GenServer
   def init(_args) do
-    {:ok, nil}
+    {:ok, %{attempts: 0, successes: 0, failures: 0}}
+  end
+
+  @spec stats :: state()
+  def stats do
+    GenServer.call(__MODULE__, :stats)
   end
 
   @spec fcm_request(map()) :: :ok
-  def fcm_request(request) do
-    GenServer.cast(__MODULE__, {:request, request})
+  def fcm_request(payload) do
+    msg = {:request, payload}
+    :gproc.send({:p, :l, :fcm_queue_monitor}, msg)
+    GenServer.cast(__MODULE__, msg)
   end
 
-  @spec fcm_response(fcm_result()) :: :ok
-  def fcm_response(response) do
-    GenServer.cast(__MODULE__, {:response, response})
+  @spec fcm_response({:ok, HTTPoison.Response.t()} | {:error, HTTPoison.Error.t()}, map()) :: :ok
+  def fcm_response(response, payload) do
+    msg = {:response, response, payload}
+    :gproc.send({:p, :l, :fcm_queue_monitor}, msg)
+    GenServer.cast(__MODULE__, msg)
   end
 
-  def handle_cast(_request, state) do
-    {:noreply, state}
+  @impl GenServer
+  def handle_call(:stats, _from, state) do
+    {:reply, state, state}
+  end
+
+  @impl GenServer
+  def handle_cast({:request, _payload}, state) do
+    {:noreply, %{state | attempts: state[:attempts] + 1}}
+  end
+
+  @impl GenServer
+  def handle_cast({:response, {:ok, %HTTPoison.Response{status_code: 200}}, _payload}, state) do
+    {:noreply, %{state | successes: state[:successes] + 1}}
+  end
+
+  @impl GenServer
+  def handle_cast({:response, _, _}, state) do
+    {:noreply, %{state | failures: state[:failures] + 1}}
   end
 end
